63a4581b04a9eac25267fed41e15b2af82ecee28
tanzellgo <tanzell.go@obf.ateneo.edu>
2020-07-06 19:25:03 +0800

    first commit

---
+++ json-server/public/precache-manifest.b00b1248458d5e3e55fb6daf17c82255.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c603d259577c91199a529765ec2db0ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "729d118183c4b21f2eb7",
+    "url": "/static/css/2.c21e1fb8.chunk.css"
+  },
+  {
+    "revision": "131a901c05f63f1e673a",
+    "url": "/static/css/main.f38e0cd8.chunk.css"
+  },
+  {
+    "revision": "729d118183c4b21f2eb7",
+    "url": "/static/js/2.d0d5f74f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d0d5f74f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131a901c05f63f1e673a",
+    "url": "/static/js/main.2bbe0b97.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
